fdef7a92d905ec228a4bf78a86b9270b9783449d
Deepa Kajjera <141205460+kajjera1@users.noreply.github.com>
2024-06-22 18:39:17 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,16 +1,29 @@
-#installing git java-1.8.0 maven
-yum install git java-1.8.0-openjdk maven -y
+#Update the package index
+sudo yum update -y
+
+#installing git 
+sudo yum install git -y
+
+#Install Maven
+sudo yum install maven -y
+
+#Install Java 1.8.0 (OpenJDK)
+sudo yum install openjdk-8-jdk -y
+
+#Install Java (Jenkins requires Java)
+sudo apt install openjdk-11-jdk
 
 #Getting the repo (jenkines.io-> download-> readhat)
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
   sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 # download java11 and jenkines 
-#amazon-linux-extras -> use this when you have different version of java in server
-amazon-linux-extras install java-openjdk11 -y
-yum install jenkins -y
-update-alternatives --config java
+sudo yum install jenkins -y
 
 # restating and check status of jenkins
-systemctl start jenkins.service
-systemctl status jenkins.service
+systemctl start jenkins
+systemctl status jenkins
+
+#Access Jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
